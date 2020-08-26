008d765bafe9a7bc2bbb20534d8b0f434acb1035
lim <lim1202@gmail.com>
2020-08-26 12:37:34 +0800

    Use feign client

--- spring-cloud/spring-cloud-nacos-consumer/src/main/java/com/example/nacos/NacosConsumerApplication.java
+++ spring-cloud/spring-cloud-nacos-consumer/src/main/java/com/example/nacos/NacosConsumerApplication.java
@@ -4,16 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class NacosConsumerApplication {
 
 	public static void main(String[] args) {
@@ -24,28 +25,23 @@ public class NacosConsumerApplication {
     public class NacosController{
 
         @Autowired
-        private LoadBalancerClient loadBalancerClient;
-        @Autowired
-        private RestTemplate restTemplate;
+        NacosProviderService nacosProviderService;
 
         @Value("${spring.application.name}")
         private String appName;
 
         @GetMapping("/echo/app-name")
         public String echoAppName(){
-            //使用 LoadBalanceClient 和 RestTemolate 结合的方式来访问
-            ServiceInstance serviceInstance = loadBalancerClient.choose("spring-cloud-nacos-provider");
-            String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
-            System.out.println("request url:"+url);
-            return restTemplate.getForObject(url,String.class);
+            return nacosProviderService.echo(appName);
         }
+    }
 
-        //实例化 RestTemplate 实例
-        @Bean
-        public RestTemplate restTemplate(){
-
-            return new RestTemplate();
-        }
+    @Service
+    @FeignClient(name = "spring-cloud-nacos-provider")
+    public interface NacosProviderService {
+    
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string);
     }
 
 }
